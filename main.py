cb06aac034a348a203fdbefeb214af8a84243ba8
Alandouglas Godinho Mendes <adgmendes@gmail.com>
2023-05-30 15:48:03 -0300

    added scrap

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 def foo():
     print('ello foo')
+    
+async def scrap(urltoScrap):
+    browser = await launch(executablePath='/usr/bin/google-chrome-stable', headless=True, args=['--no-sandbox'])
+    page = await browser.newPage()
+    await page.goto(urltoScrap, {'waitUntil': 'networkidle2'})
+    await page.waitForFunction('document.readyState === "complete"')
+    htmlContent = await page.content()
+    await browser.close()
+    return htmlContent
